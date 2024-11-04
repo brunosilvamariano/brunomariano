6b9bb2c6f98b08d46d672eac545ff3655416e683
Bruno Silva <brunomariano.sv@gmail.com>
2024-11-03 22:56:27 -0300

    Rafaturando os códigos das seções

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,12 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const prices = document.querySelectorAll(".price-3d");
-    prices.forEach((price) => {
-        price.style.transition = "transform 0.3s";
-        price.addEventListener("mouseenter", () => {
-            price.style.transform = "scale(1.1)";
-        });
-        price.addEventListener("mouseleave", () => {
-            price.style.transform = "scale(1)";
-        });
+document.querySelectorAll('.faq-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqItem = button.closest('.faq-item');
+        faqItem.classList.toggle('active');
     });
 });
